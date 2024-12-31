cd15dd90e2217553fc5e537a1a8c9053d2c03cff
Donatas Glodenis <dg@lapas.info>
2024-12-31 10:59:53 +0200

    add more colours to icons

--- themes/App/Bonfire2Home/Views/_content.php
+++ themes/App/Bonfire2Home/Views/_content.php
@@ -1,5 +1,6 @@
 <div class="col-md-9">
-        <x-content-card id="welcome" title="Welcome to Bonfire2" subtitle="Admin panel and application skeleton for CodeIgniter4 framework">
+
+    <x-content-card id="welcome" title="Welcome to Bonfire2" subtitle="Admin panel and application skeleton for CodeIgniter4 framework" >
         <p class="card-text">Bonfire is a robust application skeleton for CodeIgniter 4-based applications. It provides a number of helpful libraries to assist you in making better software for your clients, faster, while allowing you to focus on the new parts that matter to each specific application.</p>
         <p class="card-text">Currently, it includes the following features:</p>
         <div class="row">
@@ -92,18 +93,20 @@
         </div>
     </x-content-card>
 
-    <x-content-card id="philosophy" title="Philosophy">
+    <x-content-card id="philosophy" title="Philosophy" >
         <p class="card-text">We believe that the best tools don't get in your way. They don't fight you, or make you jump through hoops to get your work done. But they also don't do everything for you. They provide a solid foundation, and then get out of the way.</p>
         <p class="card-text">We believe that a product can only be as easy to use as its documentation. If you can't figure out how to use it, then it's not a good tool.</p>
         <p class="card-text">We also know that we don't have all the answers. We don't know your specific needs, or the best way to solve your problems. Therefore the tool should be flexible enough to allow you to solve your problems in the way that makes the most sense to you.</p>
         <p class="card-text">We believe we're going to get it wrong along the way. Paths that we're sure are the right ones will turn out to be dead ends. We're going to need to change direction. But when we do it should be in a way that doesn't break your work.</p>
         <p class="card-text">We believe that the best tools are not created in a silo. They are created with the input and feedback of the people that use them. The community is a vital part of the process, from writing documentation, to testing, to contributing code and bug fixes. We are in this together.</p>
     </x-content-card>
-    <x-content-card id="installation" title="Installation">
+
+    <x-content-card id="installation" title="Installation" >
         <p class="card-text">Bonfire is a set of modules and libraries for CodeIgniter 4. It is designed to be installed via <a href="https://getcomposer.org">Composer</a> and is not intended to be installed manually. We assume that you will be doing that in a Windows or Unix machine with at least php and Composer <a href="https://getcomposer.org/doc/00-intro.md#globally">installed globally</a>.</p>
         <p class="card-text">Detailed install instructions can be found <a href="https://lonnieezell.github.io/Bonfire2/intro/installation/">in Bonfire2 documentation</a>.</p>
     </x-content-card>
-    <x-content-card id="third-party" title="Third Party Software Used">
+
+    <x-content-card id="third-party" title="Third Party Software Used" >
         <ul>
             <li><a href="https://getbootstrap.com/">Bootstrap 5</a> for the CSS foundation</li>
             <li><a href="https://fontawesome.com/">FontAwesome 6</a> icons used in the admin</li>
@@ -114,4 +117,5 @@
             <li><a href="https://github.com/codeigniter4/settings">CodeIgniter/Settings</a> Database config layer (originally developed for Bonfire)</li>
         </ul>
     </x-content-card>
+
 </div>
