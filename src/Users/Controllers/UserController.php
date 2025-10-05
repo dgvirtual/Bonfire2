a28374a8cf6d42d7f706f3d8ceee6bfbc2cb77bd
Donatas Glodenis <dg@lapas.info>
2025-10-05 14:27:07 +0300

    Enable eager-loading for user data (prevents N+1 queries)

--- src/Users/Controllers/UserController.php
+++ src/Users/Controllers/UserController.php
@@ -39,7 +39,11 @@ class UserController extends AdminController
         /** @var UserFilter $userModel */
         $userModel = model(UserFilter::class);
 
-        $userModel->filter($this->request->getGet('filters'));
+        $userModel->filter($this->request->getGet('filters'))
+            // TODO: withPermissions() does not prevent lazy loading, investigate with Shield team
+            ->withPermissions()
+            ->withIdentities()
+            ->withGroups();
 
         $view = $this->request->hasHeader('HX-Request')
             ? $this->viewPrefix . '_table'
